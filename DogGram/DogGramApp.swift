5666bca0b87a2597091b9d756a67b3e08591692e
Daryl <daryl.9@gmail.com>
2021-09-06 18:34:10 +0800

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by admin on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
